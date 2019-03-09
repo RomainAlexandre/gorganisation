634bd0359860c7f912f0210275a4a41170d32d3f
Romain Alexandre <alexandre.romain06@gmail.com>
2019-03-09 13:26:05 +0000

    Add User model and action

---
+++ actions/users.go
@@ -0,0 +1,152 @@
+package actions
+
+import (
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
+	"github.com/pkg/errors"
+	"github.com/romainalexandre/gorganisation/models"
+)
+
+// Following naming logic is implemented in Buffalo:
+// Model: Singular (User)
+// DB Table: Plural (users)
+// Resource: Plural (Users)
+// Path: Plural (/users)
+
+// UsersResource is the resource for the User model
+type UsersResource struct {
+	buffalo.Resource
+}
+
+// List gets all Users. This function is mapped to the path
+// GET /users
+func (v UsersResource) List(c buffalo.Context) error {
+	// Get the DB connection from the context
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.WithStack(errors.New("no transaction found"))
+	}
+
+	users := &models.Users{}
+
+	// Paginate results. Params "page" and "per_page" control pagination.
+	// Default values are "page=1" and "per_page=20".
+	q := tx.PaginateFromParams(c.Params())
+
+	// Retrieve all Users from the DB
+	if err := q.All(users); err != nil {
+		return errors.WithStack(err)
+	}
+
+	return c.Render(200, r.JSON(users))
+}
+
+// Show gets the data for one User. This function is mapped to
+// the path GET /users/{user_id}
+func (v UsersResource) Show(c buffalo.Context) error {
+	// Get the DB connection from the context
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.WithStack(errors.New("no transaction found"))
+	}
+
+	// Allocate an empty User
+	user := &models.User{}
+
+	// To find the User the parameter user_id is used.
+	if err := tx.Find(user, c.Param("user_id")); err != nil {
+		return c.Error(404, err)
+	}
+
+	return c.Render(200, r.JSON(user))
+}
+
+// Create adds a User to the DB. This function is mapped to the
+// path POST /users
+func (v UsersResource) Create(c buffalo.Context) error {
+	// Allocate an empty User
+	user := &models.User{}
+
+	// Bind user to the json payload
+	if err := c.Bind(user); err != nil {
+		return errors.WithStack(err)
+	}
+
+	// Get the DB connection from the context
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.WithStack(errors.New("no transaction found"))
+	}
+
+	// Validate the data from the json payload
+	verrs, err := user.Create(tx)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if verrs.HasAny() {
+		return c.Render(422, r.JSON(verrs))
+	}
+
+	// and redirect to the users index page
+	return c.Render(201, r.JSON(user))
+}
+
+// Update changes a User in the DB. This function is mapped to
+// the path PUT /users/{user_id}
+func (v UsersResource) Update(c buffalo.Context) error {
+	// Get the DB connection from the context
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.WithStack(errors.New("no transaction found"))
+	}
+
+	// Allocate an empty User
+	user := &models.User{}
+
+	if err := tx.Find(user, c.Param("user_id")); err != nil {
+		return c.Error(404, err)
+	}
+
+	// Bind User to the json payload elements
+	if err := c.Bind(user); err != nil {
+		return errors.WithStack(err)
+	}
+
+	verrs, err := user.Update(tx)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if verrs.HasAny() {
+		return c.Render(422, r.JSON(verrs))
+	}
+
+	// Returns updated user
+	return c.Render(200, r.JSON(user))
+}
+
+// Destroy deletes a User from the DB. This function is mapped
+// to the path DELETE /users/{user_id}
+func (v UsersResource) Destroy(c buffalo.Context) error {
+	// Get the DB connection from the context
+	tx, ok := c.Value("tx").(*pop.Connection)
+	if !ok {
+		return errors.WithStack(errors.New("no transaction found"))
+	}
+
+	// Allocate an empty User
+	user := &models.User{}
+
+	// To find the User the parameter user_id is used.
+	if err := tx.Find(user, c.Param("user_id")); err != nil {
+		return c.Error(404, err)
+	}
+
+	if err := tx.Destroy(user); err != nil {
+		return errors.WithStack(err)
+	}
+
+	// Return deleted user
+	return c.Render(200, r.JSON(user))
+}
---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/romainalexandre/gorganisation
+
+require (
+	github.com/codegangsta/negroni v1.0.0 // indirect
+	github.com/gobuffalo/buffalo v0.14.0
+	github.com/gobuffalo/buffalo-plugins v1.13.1 // indirect
+	github.com/gobuffalo/buffalo-pop v1.8.0
+	github.com/gobuffalo/envy v1.6.15
+	github.com/gobuffalo/genny v0.0.0-20190228211236-f262bb0c783b // indirect
+	github.com/gobuffalo/mw-contenttype v0.0.0-20190224202710-36c73cc938f3
+	github.com/gobuffalo/mw-csrf v0.0.0-20190129204204-25460a055517 // indirect
+	github.com/gobuffalo/mw-forcessl v0.0.0-20190224202501-6d1ef7ffb276
+	github.com/gobuffalo/mw-paramlogger v0.0.0-20190224201358-0d45762ab655
+	github.com/gobuffalo/packr v1.22.0
+	github.com/gobuffalo/packr/v2 v2.0.3
+	github.com/gobuffalo/pop v4.9.9+incompatible
+	github.com/gobuffalo/suite v2.6.0+incompatible
+	github.com/gobuffalo/syncx v0.0.0-20190224160051-33c29581e754 // indirect
+	github.com/gobuffalo/uuid v2.0.5+incompatible
+	github.com/gobuffalo/validate v2.0.3+incompatible
+	github.com/gobuffalo/x v0.0.0-20190224155809-6bb134105960
+	github.com/golang/protobuf v1.3.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/markbates/grift v1.0.5
+	github.com/mattn/go-colorable v0.1.1 // indirect
+	github.com/mattn/go-isatty v0.0.6 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/rs/cors v1.6.0
+	github.com/unrolled/secure v1.0.0
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
+	golang.org/x/net v0.0.0-20190301231341-16b79f2e4e95 // indirect
+	golang.org/x/sys v0.0.0-20190308023053-584f3b12f43e // indirect
+	golang.org/x/tools v0.0.0-20190308174544-00c44ba9c14f // indirect
+)
