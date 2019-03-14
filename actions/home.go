3c2d4e78e3feb57ae653bab6933c3b855d991383
Romain Alexandre <alexandre.romain06@gmail.com>
2019-03-14 08:14:33 +0000

    Add React to build a small user interface

--- actions/home.go
+++ actions/home.go
@@ -2,17 +2,17 @@ package actions
 
 import (
 	"github.com/gobuffalo/buffalo"
-	"github.com/romainalexandre/gorganisation/models"
 )
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	var message string
-	if currentUser, ok := c.Data()["currentUser"].(*models.User); ok {
-		message = "Welcome to Bufallo: " + currentUser.Name
-	} else {
-		message = "Welcome to Bufallo! Please Log in"
-	}
-	return c.Render(200, r.JSON(map[string]string{"message": message}))
+	// var message string
+	// if currentUser, ok := c.Data()["currentUser"].(*models.User); ok {
+	// 	message = "Welcome to Bufallo: " + currentUser.Name
+	// } else {
+	// 	message = "Welcome to Bufallo! Please Log in"
+	// }
+	// return c.Render(200, r.JSON(map[string]string{"message": message}))
+	return c.Render(200, r.HTML("index.html"))
 }
--- actions/render.go
+++ actions/render.go
@@ -2,12 +2,18 @@ package actions
 
 import (
 	"github.com/gobuffalo/buffalo/render"
+	"github.com/gobuffalo/packr/v2"
 )
 
 var r *render.Engine
+var assetsBox = packr.New("app:assets", "../public")
 
 func init() {
 	r = render.New(render.Options{
 		DefaultContentType: "application/json",
+
+		// Box containing all of the templates:
+		TemplatesBox: packr.New("app:templates", "../templates"),
+		AssetsBox:    assetsBox,
 	})
 }
